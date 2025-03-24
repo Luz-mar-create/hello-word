3c451168e02763a399ff3784f8c8672df9e69ef9
Luz-mar-create <luzzzz7@gmail.com>
2025-03-24 14:37:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
-include 'config.php';
-
-$sql = "SELECT message FROM messages LIMIT 1";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    echo "<h1>" . $row["message"] . "</h1>";
-} else {
-    echo "<h1>Aucun message trouvé</h1>";
-}
-
-$conn->close();
+echo "Hello, World!";
 ?>
+
